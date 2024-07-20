2c3b751d27bcc779410a5a144bedea2d50c4b348
Vladimir Amirov <vladimirami@nitka.com>
2024-07-20 09:40:05 +0500

    added variables

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "vpc_cidr_block" {
+  type = string
+  default = "192.168.0.0/16"
+}
+
+variable "region" {
+  type = string
+  default = "us-east-1"
+}
+
+variable "eks_name" {
+  type = string
+  default = "datarails-demo"
+}
+
+variable "instance_type" {
+  type = string
+  default = "t3.medium"
+}
+
+variable "template" {
+  type = string
+  default = "01000000-0000-4000-8000-000030080200"
+}
+
+variable "template" {
+  type = string
+  default = "01000000-0000-4000-8000-000030080200"
+}
+
+variable "template" {
+  type = string
+  default = "01000000-0000-4000-8000-000030080200"
+}
